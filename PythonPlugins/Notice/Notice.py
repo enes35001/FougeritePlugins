0ea501848cad15d33332f4391be7ff0653a548b9
jakkee <stat_changer1@hotmail.com>
2015-02-26 23:17:36 +0800

    Removed untiyengine

---
+++ PythonPlugins/Notice/Notice.py
@@ -0,0 +1,13 @@
+__title__ = 'Notice'
+__author__ = 'Jakkee'
+__version__ = '1.0'
+
+import clr
+clr.AddReferenceByPartialName("Fougerite")
+import Fougerite
+
+
+class Notice:
+    def On_PlayerConnected(self, Player):
+        Player.SendCommand("deathscreen.reason " + '"' + "              WELCOME TO XHTs DEV SERVER!                         We test plugins on here first!                                    Developed by Jakkee. (Also you're not dead)" + '"')
+        Player.SendCommand("deathscreen.show")
