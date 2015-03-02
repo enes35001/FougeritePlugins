7b0a84748cbd9aa047dfd0737948bb99fb8a96b1
jakkee <stat_changer1@hotmail.com>
2015-03-02 18:52:06 +0800

    Small Changes

--- PythonPlugins/DonatorRank/DonatorRank.py
+++ PythonPlugins/DonatorRank/DonatorRank.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __title__ = 'DonatorRank'
 __author__ = 'Jakkee'
-__version__ = '1.4.2'
+__version__ = '1.4.3'
  
 import clr
 clr.AddReferenceByPartialName("Fougerite")
@@ -1114,9 +1114,7 @@ class DonatorRank:
     def isBanned(self, Player):
         try:
             ini = self.BansListIni()
-            if ini.GetSetting("BannedIPs", Player.IP) is not None:
-                return True
-            elif ini.GetSetting("BannedIDs", Player.SteamID) is not None:
+            if ini.GetSetting("BannedIDs", Player.SteamID) is not None:
                 return True
             else:
                 return None
@@ -1154,7 +1152,6 @@ class DonatorRank:
             elif DataStore.Get("ModBan", hit.Attacker.SteamID) == "on":
                 ini = self.BansListIni()
                 hit.DamageAmount = 0
-                ini.AddSetting("BannedIPs", hit.Victim.IP, hit.Victim.Name + " was banned by: " + hit.Attacker.Name)
                 ini.AddSetting("BannedIDs", hit.Victim.SteamID, hit.Victim.Name + " was banned by: " + hit.Attacker.Name)
                 ini.Save()
                 Server.Broadcast("[color red]" + hit.Victim.Name + "[/color] has been banned by: [color red]" + hit.Attacker.Name)
